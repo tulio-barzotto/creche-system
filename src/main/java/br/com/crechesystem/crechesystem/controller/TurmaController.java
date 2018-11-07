0864cd1af6c300765ef4af79e22adba56a157e6b
Tulio Barzotto <tulio.barzotto@gmail.com>
2018-11-06 23:23:10 -0200

    Implementação endpoint getAll turmas

--- src/main/java/br/com/crechesystem/crechesystem/controller/TurmaController.java
+++ src/main/java/br/com/crechesystem/crechesystem/controller/TurmaController.java
@@ -2,6 +2,7 @@ package br.com.crechesystem.crechesystem.controller;
 
 import br.com.crechesystem.crechesystem.domain.Turma;
 import br.com.crechesystem.crechesystem.service.TurmaService;
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -30,10 +34,19 @@ public class TurmaController {
 
     @GetMapping("/{id}")
     public ResponseEntity getTurmaById(@PathVariable Long id) {
-        String pw_hash = BCrypt.hashpw("senha123", BCrypt.gensalt());
         LOGGER.info("Requisição Turma por ID: {}", id);
         Optional<Turma> turma = turmaService.findOne(id);
         return turma.map(c -> new ResponseEntity<>(c, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Turma>> getAll() {
+        List<Turma> turmas = turmaService.findAll();
+        if(CollectionUtils.isNotEmpty(turmas)) {
+            return new ResponseEntity<>(turmas, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(Collections.emptyList(), HttpStatus.OK);
+        }
     }
 }
